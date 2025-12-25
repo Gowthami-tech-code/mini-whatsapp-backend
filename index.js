6fe7bc9d72247060b4678895c08783f459024069
gowthami <gowthami.nellipudi@acet.in>
2025-12-25 22:14:37 +0530

    added vercel url

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mini-whatsapp.vercel.app",
     methods: ["GET", "POST"]
   }
 });
